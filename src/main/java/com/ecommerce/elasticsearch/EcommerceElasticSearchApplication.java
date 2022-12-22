68ecaf450f8a5d5cb6dda428b4ee32a989f3e745
dibyajyoti behera <dibyajyotibehera@dibyajyotis-MacBook-Pro-2.local>
2022-12-22 18:37:57 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/elasticsearch/EcommerceElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceElasticSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceElasticSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/elasticsearch/EcommerceElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceElasticSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
